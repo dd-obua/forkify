76f193e074d541b514c6103b630978652084d20e
Obua Denis Daniel <dd.obua@gmail.com>
2023-12-04 16:02:29 +0300

    Load recipe data according to recipe id

--- src/js/index.js
+++ src/js/index.js
@@ -17,15 +17,16 @@ const renderSpinner = (parentElement) => {
 
 const showRecipe = async () => {
   try {
+    const id = window.location.hash.slice(1);
+    if (!id) return;
+
     // Load recipe
     renderSpinner(recipeContainer);
 
-    const res = await fetch(
-      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
-    );
+    const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/${id}`);
     const data = await res.json();
     if (!res.ok) throw new Error(`${res.status} - ${data.message}`);
-    console.log(data);
+
     let { recipe } = data.data;
     recipe = {
       id: recipe.id,
@@ -138,5 +139,3 @@ const showRecipe = async () => {
     console.log(error);
   }
 };
-
-showRecipe();
