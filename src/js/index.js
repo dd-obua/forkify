04e54322d7a0f3e8039b934fb116807f54df051f
Obua Denis Daniel <dd.obua@gmail.com>
2023-12-04 12:13:33 +0300

    Fetch recipe data from herokuapp API

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,26 @@
 import '../styles/main.scss';
+
+const showRecipe = async () => {
+  try {
+    const res = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+    );
+    const data = await res.json();
+    if (!res.ok) throw new Error(`${res.status} - ${data.message}`);
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+showRecipe();
