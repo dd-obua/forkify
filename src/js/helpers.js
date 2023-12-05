5179fb38ea2eb18404f7d94aec67c3a5271a6f64
Obua Denis Daniel <dd.obua@gmail.com>
2023-12-05 16:27:02 +0300

    Set up fetch timeout

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -1,6 +1,16 @@
+import { TIMEOUT_SECONDS } from './config';
+
+const timeout = (s) => {
+  return new Promise((_, reject) => {
+    setTimeout(() => {
+      reject(new Error(`Request too long. Timeout after ${s} seconds.`));
+    }, s * 1000);
+  });   
+};
+
 export const getJSON = async (url) => {
   try {
-    const res = await fetch(url);
+    const res = await Promise.race([fetch(url), timeout(TIMEOUT_SECONDS)]);
     const data = await res.json();
     if (!res.ok) throw new Error(`${res.status} - ${data.message}`);
     return data;
