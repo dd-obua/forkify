629a28eb6b04867068b75c8d445d4825add967ed
Obua Denis Daniel <dd.obua@gmail.com>
2023-12-06 14:25:48 +0300

    Correct search input field

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -2,13 +2,13 @@ class SearchView {
   #parentElement = document.querySelector('.search');
 
   getQuery() {
-    const query = this.#parentElement.querySelector('.search_field').value;
+    const query = this.#parentElement.querySelector('.search__field').value;
     this.#clearInput();
     return query;
   }
 
   #clearInput() {
-    this.#parentElement.querySelector('.search_field').value = '';
+    this.#parentElement.querySelector('.search__field').value = '';
   }
 
   addHandlerSearch(handler) {
