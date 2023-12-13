ec1106d945b503314ee1965540ae38ff6b6be2ac
Obua Denis Daniel <dd.obua@gmail.com>
2023-12-13 10:48:17 +0300

    Select HTML elements

---
+++ src/js/views/addRecipeView.js
@@ -0,0 +1,12 @@
+import View from './view';
+import icons from '../../images/icons.svg';
+
+class AddRecipeView extends View {
+  _parentElement = document.querySelector('.upload');
+  _window = document.querySelector('.add-recipe-window');
+  _overlay = document.querySelector('.overlay');
+  _btnOpen = document.querySelector('.nav__btn--add-recipe');
+  _btnClose = document.querySelector('.btn--close-modal');
+}
+
+export default new AddRecipeView();
